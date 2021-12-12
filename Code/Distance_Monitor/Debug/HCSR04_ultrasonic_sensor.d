bd780e5e8c4cfb39aacedfed8e8748bea02fbf24
AhmedAlaa2024 <ahmedalaa.com2024@gmail.com>
2021-12-12 20:26:34 +0200

    Upload all the project's files

---
+++ Code/Distance_Monitor/Debug/HCSR04_ultrasonic_sensor.d
@@ -0,0 +1,13 @@
+HCSR04_ultrasonic_sensor.o HCSR04_ultrasonic_sensor.o:  \
+ ../HCSR04_ultrasonic_sensor.c ../HCSR04_ultrasonic_sensor.h \
+  ../std_types.h ../icu.h ../gpio.h ../lcd.h
+
+../HCSR04_ultrasonic_sensor.h:
+
+../std_types.h:
+
+../icu.h:
+
+../gpio.h:
+
+../lcd.h:
---
+++ Code/Distance_Monitor/Debug/main.d
@@ -0,0 +1,10 @@
+main.o main.o: ../main.c ../HCSR04_ultrasonic_sensor.h ../std_types.h \
+  ../lcd.h ../common_macros.h
+
+../HCSR04_ultrasonic_sensor.h:
+
+../std_types.h:
+
+../lcd.h:
+
+../common_macros.h:
